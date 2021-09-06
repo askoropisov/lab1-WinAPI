b1f1946422d4e82de05032ee78b06f18b5ee0dc3
askoropisov <askoropisov@s-terra.ru>
2021-09-06 16:00:00 +0300

    добавил отображение по слоям и обработку кнопок, вынес открываеймый файл в глобальную область видимости

--- Source.cpp
+++ Source.cpp
@@ -11,10 +11,9 @@ const wchar_t windowClass[] = _T("win32app");
 const wchar_t windowTitle[] = _T("Win32API - Layer Simulator");
 
 
-HMENU hMenubar = nullptr;
-HMENU hMenu = nullptr;
+
 OPENFILENAME ofn;
-ifstream infile;
+ifstream file;
 
 enum color {
     red,
@@ -48,12 +47,20 @@ bool read_file(ifstream &file) {
     while (true) {
         file >> token;
         if (token == "RECT") continue;
-        /*Metal met1;
+        Metal met1;
         met1.firts_angle_x = 50;
         met1.firts_angle_y = 50;
         met1.second_angle_x = 250;
         met1.second_angle_y = 300;
-        vec_met.push_back(met1);*/
+        vec_met.push_back(met1);
+
+        Poly pol;
+        pol.firts_angle_x = 400;
+        pol.firts_angle_y = 400;
+        pol.second_angle_x = 650;
+        pol.second_angle_y = 550;
+        vec_poly.push_back(pol);
+
         break;
     }
 
@@ -65,11 +72,14 @@ bool save_file(ifstream& file) {
     return true;
 }
 
+bool draw_metall = true, draw_poly = true;                                            //mb need make global
+
 LRESULT __stdcall WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 
     PAINTSTRUCT ps;
     HDC hdc;
     RECT r;
+    
 
     GetClientRect(hWnd, &r);
 
@@ -77,24 +87,31 @@ LRESULT __stdcall WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
-    case WM_CREATE:
-        hMenubar = CreateMenu();
-        hMenu = CreateMenu();
+    case WM_LBUTTONDOWN:
+        break;
+    case WM_RBUTTONUP:
+        break;
+    case WM_CREATE: {
+        HMENU hMenubar = CreateMenu();
+        HMENU hMenu = CreateMenu();
+        HMENU hMenu1 = CreateMenu();
 
-        //AppendMenu(hMenu, MF_STRING, 4131, L"&Quit");
-        AppendMenu(hMenu, MF_STRING, 100, _T("&Open file"));
-        AppendMenu(hMenu, MF_STRING, 2, _T("&Save As"));
         AppendMenu(hMenubar, MF_POPUP, (UINT_PTR)hMenu, _T("&File"));
-
+        AppendMenu(hMenu, MF_STRING, 101, _T("&Open file"));
+        AppendMenu(hMenu, MF_STRING, 102, _T("&Save As"));
+       
+        AppendMenu(hMenubar, MF_POPUP, (UINT_PTR)hMenu1, _T("&Draw"));
+        AppendMenu(hMenu1, MF_STRING, 201, _T("&Visible/invisible metall"));
+        AppendMenu(hMenu1, MF_STRING, 202, _T("&Visible/invisible poly"));
 
         SetMenu(hWnd, hMenubar);
-
         break;
+        }
     case WM_COMMAND:
         switch (LOWORD(wParam))
         {
-        case 100:
-            wchar_t szFileName[MAX_PATH] = L"";
+        case 101: {                          //Open file
+            wchar_t szFileName[MAX_PATH] = _T("");
             ZeroMemory(&ofn, sizeof(ofn));
             ofn.lStructSize = sizeof(ofn);
             ofn.hwndOwner = hWnd;
@@ -105,66 +122,94 @@ LRESULT __stdcall WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             ofn.lpstrDefExt = _T("txt");
 
             if (GetOpenFileName(&ofn)) {
-                //MessageBox(hWnd, ofn.lpstrFile, _T("File name"), MB_OK);
                 string name_file;
-                name_file = CW2A(ofn.lpstrFile);                                                //LPWSTR to string for open file
-                ifstream file(name_file);
+                name_file = CW2A(ofn.lpstrFile);                                                    //LPWSTR to string for open file
+                file.open(name_file);
 
                 if (!read_file(file)) {
+                    MessageBox(hWnd, _T("Unable to open the file"), _T("ERROR"), MB_OK | MB_ICONERROR);
                     return EXIT_FAILURE;
                 }
-                
             }
-
+            }
             break;
-        case 2:
-            save_file(infile);
+        case 102: {                          //Save as
+            const int result = MessageBox(hWnd, _T("Are you sure you want to save the changes?"), _T("Save As"), MB_YESNOCANCEL | MB_ICONQUESTION | MB_APPLMODAL);
+            switch (result)
+            {
+            case IDYES:
+                save_file(file);
+                break;
+            case IDNO:
+                read_file(file);
+                break;
+            case IDCANCEL:
+                break;
+            }
+            break;
+            }
+        case 201:                            //Visible/invisible metall
+            if (draw_metall == true) draw_metall = false;
+            else if (draw_metall == false) draw_metall = true;
+            break;
+        case 202:                            //Visible/invisible poly
+            if (draw_poly == true) draw_poly = false;
+            else if (draw_poly == false) draw_poly = true;
             break;
         default:
             break;
         }
         break;
     case WM_PAINT:
+    {
 
-        HPEN pen, old_pen;
-        HBRUSH brush, old_brush;
-
-        //set black client rect
         PAINTSTRUCT ps;
         hdc = BeginPaint(hWnd, &ps);
+        HPEN pen = CreatePen(PS_SOLID, 1, RGB(0, 0, 0));
+        HPEN old_pen = (HPEN)SelectObject(hdc, pen);
+        HBRUSH brush = CreateSolidBrush(RGB(0, 0, 0));
+        HBRUSH old_brush = (HBRUSH)SelectObject(hdc, brush);
+
+        //set black client rect
         FillRect(hdc, &r, reinterpret_cast<HBRUSH>(GetStockObject(BLACK_BRUSH)));
 
         //draw metall in file
-        pen = CreatePen(PS_SOLID, 1, RGB(80, 0, 250));
-        old_pen = (HPEN)SelectObject(hdc, pen);
-        brush = CreateSolidBrush(RGB(80, 0, 250));
-        old_brush = (HBRUSH)SelectObject(hdc, brush);
+        if (draw_metall == true) {
+            pen = CreatePen(PS_SOLID, 1, RGB(0, 40, 255));
+            old_pen = (HPEN)SelectObject(hdc, pen);
+            brush = CreateSolidBrush(RGB(0, 40, 255));
+            old_brush = (HBRUSH)SelectObject(hdc, brush);
 
-        for (int i = 0; i < vec_met.size(); i++) {
-            Rectangle(hdc, vec_met[i].firts_angle_x, vec_met[i].firts_angle_y, vec_met[i].second_angle_x, vec_met[i].second_angle_y);
+            for (int i = 0; i < vec_met.size(); i++) {
+                Rectangle(hdc, vec_met[i].firts_angle_x, vec_met[i].firts_angle_y, vec_met[i].second_angle_x, vec_met[i].second_angle_y);
+            }
+
+            SelectObject(hdc, old_pen);
+            SelectObject(hdc, old_brush);
         }
-
-        SelectObject(hdc, old_pen);
-        SelectObject(hdc, old_brush);
 
         //draw poly in file
-        pen = CreatePen(PS_SOLID, 1, RGB(250, 80, 0));
-        old_pen = (HPEN)SelectObject(hdc, pen);
-        brush = CreateSolidBrush(RGB(250, 80, 0));
-        old_brush = (HBRUSH)SelectObject(hdc, brush);
+        if (draw_poly == true) {
+            pen = CreatePen(PS_SOLID, 1, RGB(250, 80, 0));
+            old_pen = (HPEN)SelectObject(hdc, pen);
+            brush = CreateSolidBrush(RGB(250, 80, 0));
+            old_brush = (HBRUSH)SelectObject(hdc, brush);
 
-        for (int i = 0; i < vec_poly.size(); i++) {
-            Rectangle(hdc, vec_poly[i].firts_angle_x, vec_poly[i].firts_angle_y, vec_poly[i].second_angle_x, vec_poly[i].second_angle_y);
+            for (int i = 0; i < vec_poly.size(); i++) {
+                Rectangle(hdc, vec_poly[i].firts_angle_x, vec_poly[i].firts_angle_y, vec_poly[i].second_angle_x, vec_poly[i].second_angle_y);
+            }
+
+            SelectObject(hdc, old_pen);
+            SelectObject(hdc, old_brush);
         }
 
-        SelectObject(hdc, old_pen);
-        SelectObject(hdc, old_brush);
-
         EndPaint(hWnd, &ps);
-
         DeleteObject(pen);
         DeleteObject(brush);
+        
+
         break;
+        }
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
@@ -211,6 +256,6 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdL
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
+    file.close();
     return msg.wParam;
 }
